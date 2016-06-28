7b374f77fc624699aae17bafba94f90e2c47393e
duanyifei <1821367759@qq.com>
2016-06-28 18:32:29 +0800

    add test function scope code

--- test_function.py
+++ test_function.py
@@ -10,7 +10,22 @@ def show_args(a, b=None, *args, **kwargs):
     print kwargs
     return
 
+#outer_arg = 0
+
+#测试嵌套函数作用域
+def outer():
+    #global outer_arg
+    outer_arg = 0
+    def inner():
+        global outer_arg
+        outer_arg = 1
+        print outer_arg
+    inner()
+    print outer_arg
+    return
+
 if __name__ == "__main__":
     #show_args(1, 2)
-    show_args(1, data=1, b=1)
+    #show_args(1, data=1, b=1)
+    outer()
     pass
