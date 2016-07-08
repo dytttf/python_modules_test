e3f7a34e1ee08341d0ffbd949182465d2fc4d4f9
duanyifei <1821367759@qq.com>
2016-07-08 18:05:56 +0800

    add tools, append some decorate code

--- test_function.py
+++ test_function.py
@@ -24,8 +24,35 @@ def outer():
     print outer_arg
     return
 
+
+import time
+#装饰器
+def timeit():
+    def wrap(func):
+        def call(*args, **kwargs):
+            print time.time()
+            print args
+            resp = func(*args, **kwargs)
+            print time.time()
+            return resp
+        return call
+    return wrap
+
+@timeit()
+def test_timeit(a):
+    print 12
+    return
+#
+#装饰器逻辑
+#test_timeit(*args)
+#    --> timeit()(test_timeit)(*args)
+#        -->wrap(test_timeit)(*args)
+#           -->call(*args)
+                    
+
 if __name__ == "__main__":
     #show_args(1, 2)
     #show_args(1, data=1, b=1)
-    outer()
+    #outer()
+    test_timeit(1)
     pass
