9858e7189f6799f384e2c679eec9769baa0f1d56
duanyifei <1821367759@qq.com>
2016-09-22 22:42:14 +0800

    add some code to test shutil.make_archive()

--- test_shutil.py
+++ test_shutil.py
@@ -9,13 +9,23 @@ print __file__
 curpath = os.path.dirname(__file__)
 
 test_dir = os.path.join(curpath, 'test')
+test_dir_1 = os.path.join(curpath, 'test_1')
+test_file = os.path.join(test_dir, 'test.txt')
+
+# 初始化测试目录和文件
+if not os.path.exists(test_dir):
+    os.mkdir(test_dir)
+
+if not os.path.exists(test_file):
+    with open(test_file, 'w') as f:
+        f.write('test')
 
 #copyfile
-shutil.copyfile(os.path.join(test_dir, 'test.txt'), os.path.join(test_dir, 'test1.txt'))
+shutil.copyfile(test_file, os.path.join(test_dir, 'test1.txt'))
 #copy
-shutil.copy(os.path.join(test_dir, 'test.txt'), os.path.join(test_dir, 'test2.txt'))
+shutil.copy(test_file, os.path.join(test_dir, 'test2.txt'))
 #copy2  复制软连接等属性
-shutil.copy2(os.path.join(test_dir, 'test.txt'), os.path.join(test_dir, 'test3.txt'))
+shutil.copy2(test_file, os.path.join(test_dir, 'test3.txt'))
 
 
 #copyfileobj
@@ -29,9 +39,17 @@ ff.close()
 
 
 #copytree  目录
-if os.path.exists(os.path.join(curpath, 'tes1t')):
-    shutil.rmtree(os.path.join(curpath, 'tes1t'))
-shutil.copytree(test_dir, os.path.join(curpath, 'tes1t'))
+if os.path.exists(test_dir_1):
+    shutil.rmtree(test_dir_1)
+
+shutil.copytree(test_dir, test_dir_1)
+
+
+# 压缩功能
+# root_dir  从此目录开始寻找
+# base_dir  需要压缩的文件
+shutil.make_archive('test', 'zip', root_dir='../',
+    base_dir='python_modules_test/test')
 
 
 #同步当前文件夹内所有test_开头的文件到指定目录
@@ -44,7 +62,7 @@ def sync_github():
     return
 
 if __name__ == "__main__":
-    sync_github()
+    # sync_github()
     pass
 
 
