80305bafb7dd736580685c20d7b6a1308ca4dfb0
duanyifei <1821367759@qq.com>
2016-10-15 16:18:09 +0800

    add some usage

--- test_argparse.py
+++ test_argparse.py
@@ -1,8 +1,12 @@
 #coding:utf8
+'''
+http://python.usyiyi.cn/python_278/library/argparse.html
+'''
 import argparse
 import sys
 parser = argparse.ArgumentParser()
 def main():
+    parser.add_argument('--frist', action="store",default="")
     parser.add_argument('--restart', default=None, help="restart qq process")
     #不加-为必选参数
     #parser.add_argument('num', default=None, help="restart qq process")
@@ -15,7 +19,7 @@ def main():
     parser.add_argument("--dest", dest="aaa")
     
     print sys.argv
-    cmd_args = parser.parse_args()
+    cmd_args = parser.parse_args(sys.argv[1:])
     print cmd_args.port
     print cmd_args.p
     print cmd_args.meta
