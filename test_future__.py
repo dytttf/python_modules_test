f69656a825bf9b4bf4c2ba2bf594048940d894fb
duanyifei <1821367759@qq.com>
2016-11-15 20:43:39 +0800

    add comment

--- test_future__.py
+++ test_future__.py
@@ -1,12 +1,12 @@
 #coding:utf8
 '''
-from __future__ import unicode_literals, \ # 3.0加入
-     division, \ # 3.0加入
-     print_function, \ # 3.0加入
+from __future__ import unicode_literals, \ # 3.0加入 未指定编码的字符默认为 unicode
+     division, \ # 3.0加入 支持整数相除保留小数
+     print_function, \ # 3.0加入 限制 print 为函数，不能再使用 pring ''
      generators, \ # 2.3加入
      nested_scopes, \ # 2.2加入
-     absolute_import, \ # 3.0加入
-     with_statement # 2.6加入
+     absolute_import, \ # 3.0加入 
+     with_statement # 2.6加入 支持 with 语法
 
 from __future__ import unicode_literals, \
      division, \
