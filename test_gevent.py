68afd39d8f506e15665761c0347ee186fd9c15b4
duanyifei <1821367759@qq.com>
2017-05-02 15:16:59 +0800

    add some comment

--- test_gevent.py
+++ test_gevent.py
@@ -1,4 +1,10 @@
 #coding:utf-8
+"""
+当一个greenlet遇到IO操作时，比如访问网络，就自动切换到其他的greenlet
+等到IO操作完成，再在适当的时候切换回来继续执行。
+由于IO操作非常耗时，经常使程序处于等待状态，有了gevent为我们自动切换协程，
+就保证总有greenlet在运行，而不是等待IO。
+"""
 import time
 
 import gevent
